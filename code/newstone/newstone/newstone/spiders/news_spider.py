3a08782f4c1c591041b10aaaf09a0ed9709b4da3
Ying Wang <ying.wang01@student.adelaide.edu.au>
2020-04-22 17:21:02 +0930

    implemented the function to crawl news page titles in past 1 month

--- code/newstone/newstone/newstone/spiders/news_spider.py
+++ code/newstone/newstone/newstone/spiders/news_spider.py
@@ -1,17 +1,21 @@
 import scrapy
 
 class NewsSpider(scrapy.Spider):
-    name = "posts"
+    name = "crawltitle"
+    n_pages = 0
 
     start_urls = [
-        'https://news.com.au'
+        'https://web.archive.org/web/20200418075606/https://www.news.com.au/'
     ]
 
     def parse(self, response):
         yield {
-            'date': response.css('.date::text').get()
+            'date': response.xpath('//tbody/tr[@class="d"]/td[@class="c"]/@title').re('You are here: (.*)')
         }
         for post in response.css('h4'):
             yield {
                 'title': post.css('a::text').get()
             }
+        next_page = response.xpath('//tbody/tr[@class="d"]/td[@class="b"]/a/@href').get()
+        next_page = response.urljoin(next_page)
+        yield scrapy.Request(next_page, callback=self.parse)
