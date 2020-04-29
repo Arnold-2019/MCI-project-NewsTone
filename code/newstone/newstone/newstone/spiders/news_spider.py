ab2011bddb227f7414e44e581137b25b58fcdc23
Ying Wang <ying.wang01@student.adelaide.edu.au>
2020-04-29 21:08:47 +0930

    crawled titles from 2014 to 2020

--- code/newstone/newstone/newstone/spiders/news_spider.py
+++ code/newstone/newstone/newstone/spiders/news_spider.py
@@ -5,7 +5,7 @@ class NewsSpider(scrapy.Spider):
     n_pages = 0
 
     start_urls = [
-        'https://web.archive.org/web/20200318040729/https://www.news.com.au/'
+        'https://web.archive.org/web/20200417051114/https://www.news.com.au/'
     ]
 
     def parse(self, response):
